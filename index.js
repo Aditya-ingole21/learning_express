acc067a0fed82b54cb9c5f1f963b766e3b1ae8ed
Aditya-ingole21 <aditayingole113@gmail.com>
2025-07-11 18:37:54 +0000

    signin endpoint

--- index.js
+++ index.js
@@ -23,4 +23,26 @@ app.use(express.json());
 
  })
 
- 
+ app.post("/signin",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let founduser= null;
+
+    for (let i=0;i<users.length;i++){
+        if(users[i].username==username&&users[i].password==password)
+            founduser=users[i];
+    }
+
+    if (founduser){
+        const token = jwt.sign({
+        username:username,JWT_SECRET
+    });
+
+    res.json({
+        token:token
+    })
+    }
+
+
+ })
